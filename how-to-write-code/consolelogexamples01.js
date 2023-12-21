c539bed29921ec1440adf0d7104958f963a93a96
chrimcha <147195321+chrimcha@users.noreply.github.com>
2023-12-20 18:12:21 -0600

    Added comment: concatenates the strings

--- how-to-write-code/consolelogexamples01.js
+++ how-to-write-code/consolelogexamples01.js
@@ -2,5 +2,5 @@ console.log('Hello, JavaScript.'); //prints strain as is
 console.log(2001); // prints number as is
 console.log("What","do","commas","do?"); // commas add spaces
 console.log("Does", "adding",      "space", "matter?"); // added spaces are ignored
-console.log('Launch' + 'Code'); // a + sign puts the two strings together with no space in between
+console.log('Launch' + 'Code'); // a + sign puts the two strings together with no space in between; concatenates the strings 
 console.log("LaunchCode was founded in", 2013); // string and number are printed with a space between string and number
