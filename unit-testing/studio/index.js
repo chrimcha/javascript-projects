5fef0a6e7e640aa10254cb43186ffd3366cdbe21
chrimcha <147195321+chrimcha@users.noreply.github.com>
2024-02-08 21:11:56 -0600

    worked on studio for unit testing

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,7 +1,47 @@
 
 let launchcode = {
-  
+  organization: "nonprofit",
+  executiveDirector: "Jeff",
+  percentageCoolEmployees: 100,
+  programsOffered: ["Web Development", "Data Analysis", "Liftoff"],
+  launchOutput: function (num) {
+   if (num % 2 === 0 && num % 3 === 0) {
+        return "LaunchCode!";
+    } else if (num % 2 === 0) {
+        return "Launch!";
+    }else if (num % 3 === 0) {
+        return "Code!";
+    } 
+  }
 }
+
+// console.log(launchcode.launchOutput(3));
+
+// 
+
+ 
+// } else if (num % 5 === 0) {
+//     return "Rocks!";
+// } else if (num % 2,5 === 0) {
+//     return 'Launch Rocks!';
+// } else if (num % 3,5 === 0) {
+//     return 'Code Rocks!';
+// } else if (num % 2,3,5 === 0) {
+//     return 'LaunchCode Rocks!';
+// } else if (num % 2,3,5 !== 0) {
+//     return "Rutabagas! That doesn't work.";
+// }
+
+// while (num % 2 === 0) {
+
+//     if (num % 3 === 0) {
+//         return 'LaunchCode!';
+//     } else {
+//        return 'Launch!';
+//     }
+
+//     // return 'Launch!';
+//     }
 
 module.exports = launchcode;
 
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -4,5 +4,42 @@ const launchcode = require('../index.js');
 describe("Testing launchcode", function(){
 
   // Write your unit tests here!
-  
+  test("should have the property type called organization", function() {
+    
+    expect(launchcode.organization).toBe("nonprofit");
+  });
+
+  test("should have the property type called executiveDirector", function() {
+    
+    expect(launchcode.executiveDirector).toBe("Jeff");
+  });
+
+  test("should have the property type called percentageCoolEmployees", function() {
+    
+    expect(launchcode.percentageCoolEmployees).toBe(100);
+  });
+
+  test("should have the property type called programsOffered", function() {
+    expect(launchcode.programsOffered[0]).toBe("Web Development");
+    expect(launchcode.programsOffered[1]).toBe("Data Analysis");
+    expect(launchcode.programsOffered[2]).toBe("Liftoff");
+    expect(launchcode.programsOffered.length).toBe(3);
+  });
+
+  test("numbers divisible by 2 should return 'Launch!'", function() {
+    expect(launchcode.launchOutput(2)).toBe('Launch!');
+  });
+
+  test("numbers divisible by 3 should return 'Code!'", function() {
+    expect(launchcode.launchOutput(3)).toBe('Code!');
+  });
+
+  test("numbers divisible by 5 should return 'Rocks!'", function() {
+    expect(launchcode.launchOutput(5)).toBe('Rocks!');
+  });
+
+  test("numbers divisible by 2 and 3 should return 'LaunchCode!'", function() {
+    expect(launchcode.launchOutput(3)).toBe('LaunchCode!');
+    expect(launchcode.launchOutput(2)).toBe('LaunchCode!');
+  });
 });
