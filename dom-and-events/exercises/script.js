cd9dc7b87b588cbf85a9a57cec5d28331e9a3886
Christina Chavez <147195321+chrimcha@users.noreply.github.com>
2024-02-21 18:23:46 -0600

    finsihed DOM and Event try it's and exercises

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,7 +4,41 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
+
+    //code that changes the paragraph text when the Take off button is clicked
+    button.addEventListener("click", function() {
+        paragraph.innerHTML = "Houston, we have liftoff!";   
+    });
     
+    //or
+    // button.addEventListener('click', event => {
+    //     paragraph.innerHTML = 'Houston! We have liftoff!';
+    //  });
+
+    //code that makes Abort Mission button red when hovering over it and back to default color when hovering out of it 
+    missionAbort.addEventListener("mouseover", event => {
+        // console.log('yes');
+        missionAbort.setAttribute("style", "background-color: red;");
+    });
+
+    missionAbort.addEventListener("mouseout", event => {
+        // console.log('yes');
+        missionAbort.setAttribute("style", "");
+    });
+
+    //or
+    // missionAbort.addEventListener("mouseout", function( event ) {
+    //     event.target.style.backgroundColor = "";
+    // });
+
+    missionAbort.addEventListener("click", event => {
+        let c = window.confirm("Are you sure you want to abort the mission?");
+        // console.log(c);
+        if (c === true) {
+            paragraph.innerHTML = "Mission aborted! Space shuttle returning home";
+        }
+    })
+
 }
 
 window.addEventListener("load", init);
