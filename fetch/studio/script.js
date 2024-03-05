0bf7ab83949db6312af863006cbe0edfb5d582d6
chrimcha <147195321+chrimcha@users.noreply.github.com>
2024-03-04 20:41:50 -0600

    finsihed studio, and one bonus

--- fetch/studio/script.js
+++ fetch/studio/script.js
@@ -1,1 +1,34 @@
 //TODO: Add Your Code Below
+window.addEventListener("load", function() {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then( function(response) {
+        response.json().then( function(json) {            
+            const astronautsDiv = document.getElementById("container");
+            const greenForActive = document.getElementById("green-if-active");
+
+
+            astronautsDiv.innerHTML = `<div><h4>Member Count: ${json.length}</h4></div>`;
+            
+            for (let i = 0; i < json.length; i++) {
+                astronautsDiv.innerHTML += `
+                <div class="astronaut">
+                <div class="bio">
+                    <h3>${json[i].firstName} ${json[i].lastName}</h3>
+                    <ul>
+                    <li>Hours in space: ${json[i].hoursInSpace}</li>
+                    <li>Active: <span class="green-if-active">${json[i].active}</span></li>
+                    <li>Skills: ${json[i].skills}</li>
+                    </ul>
+                </div>
+                <img class="avatar" src="${json[i].picture}">
+                </div>`;
+// active to green
+                const greenForActive = document.getElementById("green-if-active");
+
+                console.log(json.active);
+                if (json.active === true) {
+                greenForActive.setAttribute("style", "color: green;");
+            }
+            };
+        });
+    });
+ });
